61bfeb4e584b996203a97ebb0971fee31e86df14
Dev-Zual <freelancerzual@gmail.com>
2022-02-11 10:13:20 +0600

    add function for input field

--- js/bank.js
+++ js/bank.js
@@ -1,14 +1,23 @@
+function getInputField(num) {
+  const getInputValue = document.getElementById(num);
+  const newInputText = getInputValue.value;
+  // convert input diposit value
+  const convertNewInput = parseFloat(newInputText);
+     // clear the diposit input field
+     getInputValue.value = '';
+  return convertNewInput;
+}
+
+
+
+
 document.getElementById('diposit-btn').addEventListener('click', function () {
-    // get diposit from input
-    const getDiposit = document.getElementById('input-diposit');
-    const newDipositText = getDiposit.value;
-
-    // convert input diposit value
-    const convertNewDiposit = parseFloat(newDipositText);
-
+   
+    // call the function
+  const convertNewDiposit = getInputField('input-diposit');
+  
     // call the previous value where show balance
     const preveousdipositTex = document.getElementById('diposit-balance');
-
     // get previous amount where show the balance
     const preveousDipositAmount = preveousdipositTex.innerText;
 
@@ -29,19 +38,16 @@ document.getElementById('diposit-btn').addEventListener('click', function () {
     const converNewBlance = parseFloat(newBlance);
     // sum & call the total balance
     currentBlance.innerText = converNewBlance + convertNewDiposit;
-    // clear the diposit input field
-    getDiposit.value = '';
+ 
     
     
 });
 
 // withdraw balance
 document.getElementById('widthdraw-btn').addEventListener('click', function () {
-    const newWithdraw = document.getElementById('input-widthdraw');
-    const newWithdrawAmount = newWithdraw.value;
-    //convert input new withdraw 
-    const convertNewWithdraw = parseFloat(newWithdrawAmount);
-
+    // call the function
+  const convertNewWithdraw = getInputField('input-widthdraw');
+    
     // call current withdraw
     const previousWithdraw = document.getElementById('withdraw');
   // set current withdraw
@@ -65,5 +71,5 @@ document.getElementById('widthdraw-btn').addEventListener('click', function () {
     // update balance
     currnetTotalBalance.innerText = convertTotalAmount - convertNewWithdraw;
 
-    newWithdraw.value = '';
+
 })
